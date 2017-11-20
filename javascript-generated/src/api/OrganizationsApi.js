d82042f87d66b95930a0d0cfed473594008ddea9
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-11-21 00:21:49 +0200

    Generated javascript source

--- javascript-generated/src/api/OrganizationsApi.js
+++ javascript-generated/src/api/OrganizationsApi.js
@@ -33,7 +33,7 @@
   /**
    * Organizations service.
    * @module api/OrganizationsApi
-   * @version 0.0.9
+   * @version 0.0.10
    */
 
   /**
--- javascript-generated/src/api/SessionApi.js
+++ javascript-generated/src/api/SessionApi.js
@@ -33,7 +33,7 @@
   /**
    * Session service.
    * @module api/SessionApi
-   * @version 0.0.9
+   * @version 0.0.10
    */
 
   /**
