984ec41d7b3e70c50e4932f192747f04b2ee0e22
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-11-21 00:11:57 +0200

    Generated javascript source

--- javascript-generated/src/model/Message.js
+++ javascript-generated/src/model/Message.js
@@ -36,7 +36,7 @@
   /**
    * The Message model module.
    * @module model/Message
-   * @version 0.0.3
+   * @version 0.0.4
    */
 
   /**
--- javascript-generated/src/model/Session.js
+++ javascript-generated/src/model/Session.js
@@ -36,7 +36,7 @@
   /**
    * The Session model module.
    * @module model/Session
-   * @version 0.0.3
+   * @version 0.0.4
    */
 
   /**
