1ce883c8b58c0e139772b774462c0ac7225a4dc8
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-01-22 11:01:39 +0200

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/metamind/server/rest/MessagesApi.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/metamind/server/rest/MessagesApi.java
@@ -21,7 +21,7 @@ import java.lang.Exception;
 @Api(description = "the messages API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-12-05T10:03:57.553+02:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2018-01-22T11:01:30.435+02:00")
 
 
 public interface MessagesApi  {
@@ -30,7 +30,9 @@ public interface MessagesApi  {
     
     @Consumes({ "application/json;charset&#x3D;utf-8" })
     @Produces({ "application/json;charset&#x3D;utf-8" })
-    @ApiOperation(value = "Posts new message", notes = "Posts new message", response = Message.class, tags={ "Messages" })
+    @ApiOperation(value = "Posts new message", notes = "Posts new message", response = Message.class, authorizations = {
+        @Authorization(value = "basicAuth")
+    }, tags={ "Messages" })
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "Returns message with bot's response details", response = Message.class),
         @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
--- jaxrs-spec-generated/src/main/java/fi/metatavu/metamind/server/rest/SessionsApi.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/metamind/server/rest/SessionsApi.java
@@ -21,7 +21,7 @@ import java.lang.Exception;
 @Api(description = "the sessions API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-12-05T10:03:57.553+02:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2018-01-22T11:01:30.435+02:00")
 
 
 public interface SessionsApi  {
@@ -30,7 +30,9 @@ public interface SessionsApi  {
     
     @Consumes({ "application/json;charset&#x3D;utf-8" })
     @Produces({ "application/json;charset&#x3D;utf-8" })
-    @ApiOperation(value = "Creates new session", notes = "Creates new chat session", response = Session.class, tags={ "Sessions" })
+    @ApiOperation(value = "Creates new session", notes = "Creates new chat session", response = Session.class, authorizations = {
+        @Authorization(value = "basicAuth")
+    }, tags={ "Sessions" })
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "Created session", response = Session.class),
         @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = BadRequest.class),
